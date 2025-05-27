c7b8950f8365247bac1310c189ab14a917ebfb57
ghost-robotic <harrisonhsieh8@gmail.com>
2025-05-27 23:19:16 +1000

    Add links between caves, Add forever loop so player can move between caves

--- Game/main.py
+++ Game/main.py
@@ -1,11 +1,29 @@
 from cave import Cave
 
-#create cave instances
+#create Cave instances
 cavern = Cave("Cavern")
-grotto = Cave("Grotto")
-dungeon = Cave("Dungeon")
-
-#set cave descriptions
 cavern.set_description("A damp and dirty cave.")
 
-cavern.describe()
+grotto = Cave("Grotto")
+grotto.set_description("A small cave with ancient graffiti")
+
+dungeon = Cave("Dungeon")
+dungeon.set_description("A large cave with a rack")
+
+#Cave links
+cavern.link_cave(dungeon, "south")
+dungeon.link_cave(cavern, "north")
+
+grotto.link_cave(dungeon, "east")
+dungeon.link_cave(grotto, "west")
+
+
+current_cave = cavern
+
+while True:
+    print("\n")
+    current_cave.get_details()
+    
+    command = input(">")
+
+    current_cave = current_cave.move(command)
