4f0dca50bb45e0d4301d5a74543ea58d2cfba467
ghost-robotic <harrisonhsieh8@gmail.com>
2025-05-27 10:15:32 +1000

    Add getter and setter methods for Cave attributes

--- Game/cave.py
+++ Game/cave.py
@@ -4,3 +4,11 @@ class Cave:
         self.name = name
         self.description = None
 
+    #Getter Methods
+    def get_description(self):
+        return self.description
+
+    #Setter Methods
+    def set_description(self, cave_description):
+        self.description = cave_description
+
