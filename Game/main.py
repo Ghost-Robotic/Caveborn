c40e4b48391d5e9e1a56aa6437f4de27738d99be
ghost-robotic <harrisonhsieh8@gmail.com>
2025-06-01 02:38:57 +1000

    Add command to take item and add to bag array, fight_with command now checks if item is in bag; Add Item instances

--- Game/main.py
+++ Game/main.py
@@ -1,6 +1,6 @@
 from cave import Cave
-from character import Character
 from character import Enemy, Friend
+from item import Item
 
 #create Cave instances
 cavern = Cave("Cavern")
@@ -29,22 +29,36 @@ josephine = Friend("Josephine", "A friendly bat")
 josephine.set_conversation("Gidday")
 grotto.set_character(josephine)
 
+#item
+vegemite = Item("vegemite")
+vegemite.set_description("A Wumpuses worst nightmare")
+grotto.set_item(vegemite)
+
+torch = Item("torch")
+torch.set_description("A light for the end of the tunnel")
+dungeon.set_item(torch)
+
 #main game loop
 current_cave = cavern
 dead = False
+bag = []
 
 while dead == False:
     print("\n")
     current_cave.get_details()
 
     inhabitant = current_cave.get_character()
+    item = current_cave.get_item()
 
     if inhabitant is not None:
         inhabitant.describe()
+        
+    if item is not None:
+        item.describe()
 
     command = input("> ")
     
-    #check and excecute command  given
+    #check and execute command  given
     if command.lower() in ["north", "south", "east", "west"]:
         #move to new cave
         current_cave = current_cave.move(command)
@@ -61,16 +75,24 @@ while dead == False:
             print("What will you fight with")
             fight_with = input("> ")
 
-            if inhabitant.fight(combat_item= fight_with):
-                #win message
-                print("Bravo, hero you won the fight!")
-                current_cave.set_character(None)
+            if fight_with in bag:
+                if inhabitant.fight(combat_item= fight_with) == True:
+                    #win message
+                    print("Bravo, hero you won the fight!")
+                    current_cave.set_character(None)
+                    
+                    if Enemy.enemies_to_defeat == 0:
+                        print("Congratulations, you have survived another adventure!")  
+                        dead = True                  
 
+                else:
+                    #loss message
+                    print("Scurry home, you lost the fight.")
+                    print("That's the end of the game")
+                    dead = True
+                    
             else:
-                #loss message
-                print("Scurry home, you lost the fight.")
-                print("That's the end of the game")
-                dead = True
+                print(f"You don't have a {fight_with}")
 
         else:
             print("There is no one here to fight with")
@@ -85,4 +107,12 @@ while dead == False:
 
         else:
             print(f"There is no one here to pat :(")
-
+            
+    elif command == "take":
+        if item is not None:
+            print(f"You put the {item.get_name()} in your bag")
+            bag.append(item.get_name())
+            current_cave.set_item(None)
+            
+            
+    
