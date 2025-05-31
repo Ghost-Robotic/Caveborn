d5d1d19c3251502fb4a73af4b99a28bd1190b933
ghost-robotic <harrisonhsieh8@gmail.com>
2025-06-01 00:13:06 +1000

    Recoded move command; Add talk, fight, pat commands

--- Game/main.py
+++ Game/main.py
@@ -1,6 +1,6 @@
 from cave import Cave
 from character import Character
-from character import Enemy
+from character import Enemy, Friend
 
 #create Cave instances
 cavern = Cave("Cavern")
@@ -19,16 +19,21 @@ dungeon.link_cave(cavern, "north")
 grotto.link_cave(dungeon, "east")
 dungeon.link_cave(grotto, "west")
 
-harry = Enemy(char_name= "Harry", 
-              char_description= "A smelly Wumpus")
+#characters
+harry = Enemy("Harry", "A smelly Wumpus")
 harry.set_conversation("Hangry...Hanggrry")
 harry.set_weakness("vegemite")
-
 dungeon.set_character(harry)
 
-current_cave = cavern
+josephine = Friend("Josephine", "A friendly bat")
+josephine.set_conversation("Gidday")
+grotto.set_character(josephine)
 
-while True:
+#main game loop
+current_cave = cavern
+dead = False
+
+while dead == False:
     print("\n")
     current_cave.get_details()
 
@@ -36,7 +41,48 @@ while True:
 
     if inhabitant is not None:
         inhabitant.describe()
-    
-    command = input("> ")
 
-    current_cave = current_cave.move(command)
+    command = input("> ")
+    
+    #check and excecute command  given
+    if command.lower() in ["north", "south", "east", "west"]:
+        #move to new cave
+        current_cave = current_cave.move(command)
+
+    elif command == "talk":
+        #talk to 
+        if inhabitant is not None:
+            inhabitant.talk()
+    
+    elif command == "fight":
+        #attempt to fight enemy
+        if inhabitant is not None and isinstance(inhabitant, Enemy):
+            #Fight with the inhabitant if there is one
+            print("What will you fight with")
+            fight_with = input("> ")
+
+            if inhabitant.fight(combat_item= fight_with):
+                #win message
+                print("Bravo, hero you won the fight!")
+                current_cave.set_character(None)
+
+            else:
+                #loss message
+                print("Scurry home, you lost the fight.")
+                print("That's the end of the game")
+                dead = True
+
+        else:
+            print("There is no one here to fight with")
+
+    elif command == "pat":
+        if inhabitant is not None:
+            if isinstance(inhabitant, Enemy):
+                print("I wouldn't do that if I were you...")
+
+            else:
+                inhabitant.pat()
+
+        else:
+            print(f"There is no one here to pat :(")
+
