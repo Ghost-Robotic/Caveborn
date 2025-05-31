51dd1913735f7a014f24ef638192ccd9920557ec
ghost-robotic <harrisonhsieh8@gmail.com>
2025-06-01 02:37:28 +1000

    Add name, description and rarity attributes with getter, setter methods; Add describe item

---
+++ Game/item.py
@@ -0,0 +1,30 @@
+class Item:
+    #constructor method
+    def __init__(self, name):
+        self.name = name
+        self.description = None
+        self.rarity = None
+        
+    #getter methods
+    def get_name(self):
+        return self.name
+    
+    def get_description(self):
+        return self.description
+    
+    def get_rarity(self):
+        return self.rarity
+    
+    #setter methods
+    def set_name(self, name):
+        self.name = name
+        
+    def set_description(self, description):
+        self.description = description
+        
+    def set_rarity(self, rarity):
+        self.rarity = rarity
+        
+    #describe item
+    def describe(self):
+        print(f"You spot a {self.name}- {self.description}")
