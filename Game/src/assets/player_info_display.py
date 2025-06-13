e5be4077b0bd5f09affc05c0d0ac7ae2a58a41c0
ghost-robotic <harrisonhsieh8@gmail.com>
2025-06-14 01:00:21 +1000

    Add title to display, Update compass and player info display

---
+++ Game/src/assets/player_info_display.py
@@ -0,0 +1,75 @@
+class PlayerDisplay():
+    north = None
+    east = None
+    south = None
+    west = None
+    
+    health = None
+    bag = None
+    
+    height = 7
+    
+    player_compass = []
+    player_info = []
+    
+    @classmethod
+    def update_info(cls, current_cave, health, bag):
+        cls.health = health
+        
+        if bag == []:
+            cls.bag = "Empty"            
+        else:
+            cls.bag = bag[0]
+            for i in range(len(bag)-1):
+                cls.bag = cls.bag + "\x1b[39m, \x1b[38;5;207m" + bag[i+1]    
+        
+        linked_caves = current_cave.get_linked_caves()
+        
+        if "north" in linked_caves:
+            cls.north = "\x1b[38;5;196mN\x1b[0m"       
+        else:
+            cls.north = "N"
+        
+        if "east" in linked_caves:
+            cls.east = "\x1b[38;5;196mE\x1b[0m"
+        else:
+            cls.east = "E"
+        
+        if "south" in linked_caves:
+            cls.south = "\x1b[38;5;196mS\x1b[0m"
+        else:
+            cls.south = "S"
+        
+        if "west" in linked_caves:
+            cls.west = "\x1b[38;5;196mW\x1b[0m"
+        else:
+            cls.west = "W"    
+            
+            
+    @classmethod
+    def update_display(cls):
+        cls.player_compass = [
+            f" ╔═══════════╗",                   #R1 len:12
+            f" ║     {cls.north}     ║",         #R1 
+            f" ║     ʌ     ║",                   #R2 
+            f" ║ {cls.west} < o > {cls.east} ║", #R3 
+            f" ║     v     ║",                   #R4 
+            f" ║     {cls.south}     ║",         #R5 
+            f" ╚═══════════╝"                    #R5 
+        ]
+        
+        cls.player_info = [
+            f"",
+            f" \x1b[38;5;46mHealth\x1b[0m: \x1b[1;38;5;201m{cls.health}\x1b[0m",
+            f" \x1b[38;5;39mBag\x1b[0m: [\x1b[38;5;207m{cls.bag}\x1b[0m]",
+            f" ",
+            f" ",    
+            f" ",
+            f"" 
+        ]                        
+          
+            
+    @classmethod 
+    def output(cls):
+        for line in range(cls.height):
+            print(cls.player_compass[line] , cls.player_info[line])
