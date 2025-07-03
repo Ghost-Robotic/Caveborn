31ab93d5854d684c80c969524c9aa7a9d67b9376
ghost-robotic <harrisonhsieh8@gmail.com>
2025-07-03 12:17:25 +1000

    Fix issue with top part of screen being deleted maybe?

--- Game/src/commands/commands.py
+++ Game/src/commands/commands.py
@@ -93,7 +93,8 @@ class Command():
         Command.sequential_print("Enter ", 0.02, "\x1b[38;5;226m")
         Command.sequential_print("to Continue", 0.02, "")
         input("\033[?25l\x1b[8m")
-        print('\x1b[1A \x1b[0J\033[?25h', end = '')
+        print('\x1b[1A\x1b[0J\033[?25h\x1b[0m', end = '')
+        Command.pause(0.01)
                 
         
     @staticmethod
