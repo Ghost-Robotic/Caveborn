f7acc8ccd9310d46ec0e6f952abfd75331bdbf75
ghost-robotic <harrisonhsieh8@gmail.com>
2025-09-25 16:57:37 +1000

    Remove tutorial code that was being used as a reference

--- Game/src/interface.py
+++ Game/src/interface.py
@@ -14,14 +14,6 @@ class interface_access:
     def test():
         health.configure(text="100")
 
-# def calculate(*args):
-#     try:
-#         value = float(feet.get())
-#         meters.set(int(0.3048 * value * 10000.0 + 0.5)/10000.0)
-#     except ValueError:
-#         pass
-
-# Create main window
 
 root = Tk()
 root.title("Caveborn")
@@ -145,45 +137,6 @@ command_line = Entry(command_frame,textvariable=command)
 command_line.configure(width=30, borderwidth=0, insertbackground="#ecf00c", bg="#2f3738", fg="#fbff00", font=('calibre', 13), highlightthickness=3, highlightcolor="#A2D498")
 command_line.grid(column=1, row=1, sticky=(S, W))
 
-# frame = tk.Frame(root)
-# frame.grid(column=1, row=1)
-
-# compass_font = "Arial"
-# ttk.Label(frame, text="N", font=compass_font).grid(column=3, row=1)
-# ttk.Label(frame, text="▴", font=compass_font, anchor="s").grid(column=3, row=2)
-# ttk.Label(frame, text="○", font=compass_font).grid(column=3, row=3)
-# ttk.Label(frame, text="▾", font=compass_font).grid(column=3, row=4)
-
-# ttk.Label(frame, text="W", font=compass_font).grid(column=1, row=3)
-# ttk.Label(frame, text="◂", font=compass_font).grid(column=2, row=3)
-# ttk.Label(frame, text="▸", font=compass_font).grid(column=4, row=3)
-
-# ttk.Label(frame, text="S", font=compass_font).grid(column=3, row=5)
-# ttk.Label(frame, text="E", font=compass_font).grid(column=5, row=3)
-
-# mainframe = ttk.Frame(root, padding="3 3 12 12")
-# mainframe.grid(column=0, row=1, sticky=(N, W, E, S))
-# root.columnconfigure(0, weight=1)
-# root.rowconfigure(0, weight=1)
-
-# feet = StringVar()
-# feet_entry = ttk.Entry(mainframe, width=7, textvariable=feet)
-# feet_entry.grid(column=2, row=1, sticky=(W, E))
-
-# meters = StringVar()
-# ttk.Label(mainframe, textvariable=meters).grid(column=2, row=2, sticky=(W, E))
-
-# ttk.Button(mainframe, text="Calculate", command=calculate).grid(column=3, row=3, sticky=W)
-
-# ttk.Label(mainframe, text="feet").grid(column=3, row=1, sticky=W)
-# ttk.Label(mainframe, text="is equivalent to").grid(column=1, row=2, sticky=E)
-# ttk.Label(mainframe, text="meters").grid(column=3, row=2, sticky=W)
-
-# for child in mainframe.winfo_children(): 
-#     child.grid_configure(padx=5, pady=5)
-
-# feet_entry.focus()
-# root.bind("<Return>", calculate)
 
 #command_line.focus()
 
