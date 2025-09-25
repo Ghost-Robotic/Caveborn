1992f559addf14e6c58339aaaf4f3729659637e4
ghost-robotic <harrisonhsieh8@gmail.com>
2025-09-25 12:20:35 +1000

    Add the player info sections of the interface

--- Game/src/interface.py
+++ Game/src/interface.py
@@ -78,10 +78,12 @@ title_text = tk.Text(titleframe,
 title_text.insert(tk.END, title)
 title_text.grid(column=0, row=0, padx=0, ipadx=0, ipady=0, pady=0, sticky=(N, W))
 
+mid_frame = ttk.Frame(root)
+mid_frame.grid(column=0, row=1, sticky=(N, W, E, S))
 
 # available moves compass
-compass_frame = Canvas(root, height=101, width=101, bg="#1c2526", relief="groove")
-compass_frame.grid(column=0, row=1, sticky=(N, W), padx=16, pady=16)
+compass_frame = Canvas(mid_frame, height=101, width=101, bg="#1c2526", relief="groove")
+compass_frame.grid(column=0, row=0, sticky=(N, W), padx=16, pady=16)
 
 compass_frame.create_text(52, 51, text='○', font='TkMenuFont, 14', anchor=tk.CENTER, fill = "#ffffff")
 compass_frame.create_text(52, 33, text='▲', font='TkMenuFont, 14', anchor=tk.CENTER, fill = "#ffffff")
@@ -96,8 +98,21 @@ compass_frame.create_text(14, 52, text='W', font='TkMenuFont, 12', anchor=tk.CEN
 
 
 # player information
-info_frame = ttk.Frame(root) 
-info_frame.grid(column=1, row=1, sticky=(N, W), padx=16, pady=16)
+info_frame = ttk.Frame(mid_frame, width=30, height=30) 
+info_frame.grid(column=1, row=0, sticky=(N, W), padx=16, pady=16)
+
+ttk.Label(info_frame, font=(BOLD_FONT, 12), text="Health ", foreground="#08de3d").grid(column=0, row=0, sticky=(N, W))
+ttk.Label(info_frame, font=(BOLD_FONT, 14), text=": ", foreground="#ffffff").grid(column=1, row=0, sticky=(N, W))
+ttk.Label(info_frame, font=(BOLD_FONT, 12), text="Bag ", foreground="#2ca3de").grid(column=0, row=1, sticky=(N, W))
+ttk.Label(info_frame, font=(BOLD_FONT, 14), text=": ", foreground="#ffffff").grid(column=1, row=1, sticky=(N, W))
+
+bag_box = tk.Text(info_frame, height=10, width=25)
+bag_box.grid(column=0, row=2, sticky=(N, W), columnspan=3)
+
+mode_specific_info = ttk.Frame(mid_frame)
+mode_specific_info.grid(column=2, row=0, sticky=(N, W), padx=16, pady=16)
+ttk.Label(mode_specific_info, font=(BOLD_FONT, 12), text="Enemies to defeat ", foreground="#ff0808").grid(column=0, row=0, sticky=(N, W))
+ttk.Label(mode_specific_info, font=(BOLD_FONT, 14), text=": ", foreground="#ffffff").grid(column=1, row=0, sticky=(N, W))
 
 # cave information
 cave_frame = ttk.Frame(root)
@@ -112,8 +127,7 @@ command_frame = ttk.Frame(root)
 command_frame.grid(column=0, row=3, padx=10, pady=10, sticky=(S, W))
 
 command = StringVar()
-command_symbol = ttk.Label(command_frame, text=">", font=('calibre', 16, 'bold'))
-command_symbol.grid(column=0, row=1, sticky=(S, W), padx=5)
+ttk.Label(command_frame, text=">", font=('calibre', 16, 'bold')).grid(column=0, row=1, sticky=(S, W), padx=5)
 command_line = Entry(command_frame,textvariable=command)
 command_line.configure(width=30, borderwidth=0, insertbackground="#ecf00c", bg="#2f3738", fg="#fbff00", font=('calibre', 13), highlightthickness=3, highlightcolor="#A2D498")
 command_line.grid(column=1, row=1, sticky=(S, W))
