3d7964d2f340c40b40f31cc796d3d233223e5edd
ghost-robotic <harrisonhsieh8@gmail.com>
2025-10-01 16:37:27 +1000

    Moved the different screens such as the main game screen and menu screen into their own classes. the window and the different screens are now classes

--- Game/src/interface.py
+++ Game/src/interface.py
@@ -1,20 +1,26 @@
+from ast import Load
 from tkinter import *
 from tkinter import ttk
 import tkinter as tk
 from os import system, name
 from tkinter import font
 
-class interface:
-    def __init__(self, game_mode):
-        self.root = Tk()
-        self.root.title("Caveborn")
+
+class interface(tk.Tk):
+    def __init__(self, *args, **kwargs):
+        tk.Tk.__init__(self, *args, **kwargs)
+        
+        self.NORMAL_FONT = 'Consolas 14'
+        self.BOLD_FONT = font.Font(font='Consolas', size=14, weight='bold')
+        
+        # self.root = Tk()
+        self.title("Caveborn")
         #root.iconbitmap("assets\Caveborn_Icon_2.ico")
         #root.iconphoto(True, tk.PhotoImage(file=r"assets\Caveborn_Icon_2.png"))
-        self.root.configure(bg="#1c2526")
-        system(self.root.state('zoomed') if name == 'nt' else self.root.attributes('-zoomed', True))
+        self.configure(bg="#1c2526")
+        system(self.state('zoomed') if name == 'nt' else self.attributes('-zoomed', True))
 
-        NORMAL_FONT = 'Consolas 14'
-        BOLD_FONT = font.Font(font='Consolas', size=14, weight='bold')
+
 
         # Create style
         style = ttk.Style()
@@ -45,9 +51,8 @@ class interface:
 
         style.configure("TFrame", 
                         background="#1c2526")
-
-
-        titleframe = ttk.Frame(self.root, padding="3 3 12 12")
+        
+        titleframe = ttk.Frame(self, padding="3 3 12 12")
         titleframe.grid(column=0, row=0, sticky=(N, W, E, S))
 
 
@@ -70,8 +75,31 @@ class interface:
                             wrap="none")
         title_text.insert(tk.END, title)
         title_text.grid(column=0, row=0, padx=0, ipadx=0, ipady=0, pady=0, sticky=(N, W))
+        
+        container = ttk.Frame(self)
+        container.grid(column=0, row=1, sticky=(N, W, E, S))
 
-        mid_frame = ttk.Frame(self.root)
+        self.pages = {}
+        
+        for frame in (GameFrame, MenuFrame, EndFrame, LoadingFrame):
+            page = frame(container, self)
+            
+            self.pages[frame] = page
+            
+            page.grid(column=0,row=0, sticky=(N, E, S, W))
+            
+        frame = self.pages[GameFrame]
+        frame.tkraise()
+            
+    def run_mainloop(self):
+        self.mainloop()
+        
+
+class GameFrame(ttk.Frame):
+    def __init__(self, parent, controller):
+        ttk.Frame.__init__(self, parent)
+
+        mid_frame = ttk.Frame(self)
         mid_frame.grid(column=0, row=1, sticky=(N, W, E, S))
 
         # available moves compass
@@ -95,32 +123,32 @@ class interface:
         info_frame.grid(column=1, row=0, sticky=(N, W), padx=16, pady=16)
 
         self.health = StringVar()
-        ttk.Label(info_frame, font=(BOLD_FONT, 12), text="Health ", foreground="#08de3d").grid(column=0, row=0, sticky=(N, W))
-        ttk.Label(info_frame, font=(BOLD_FONT, 14), text=": ", foreground="#ffffff").grid(column=1, row=0, sticky=(N, W))
-        health_label = ttk.Label(info_frame, font=(BOLD_FONT, 12), textvariable=self.health, foreground="#f700ff")
+        ttk.Label(info_frame, font=(controller.BOLD_FONT, 12), text="Health ", foreground="#08de3d").grid(column=0, row=0, sticky=(N, W))
+        ttk.Label(info_frame, font=(controller.BOLD_FONT, 14), text=": ", foreground="#ffffff").grid(column=1, row=0, sticky=(N, W))
+        health_label = ttk.Label(info_frame, font=(controller.BOLD_FONT, 12), textvariable=self.health, foreground="#f700ff")
         health_label.grid(column=2, row=0, sticky=(N, W))
 
-        ttk.Label(info_frame, font=(BOLD_FONT, 12), text="Bag ", foreground="#2ca3de").grid(column=0, row=1, sticky=(N, W))
-        ttk.Label(info_frame, font=(BOLD_FONT, 14), text=": ", foreground="#ffffff").grid(column=1, row=1, sticky=(N, W))
+        ttk.Label(info_frame, font=(controller.BOLD_FONT, 12), text="Bag ", foreground="#2ca3de").grid(column=0, row=1, sticky=(N, W))
+        ttk.Label(info_frame, font=(controller.BOLD_FONT, 14), text=": ", foreground="#ffffff").grid(column=1, row=1, sticky=(N, W))
 
         # bag_box = tk.Text(info_frame, height=10, width=25)
         # bag_box.grid(column=0, row=2, sticky=(N, W), columnspan=3)
 
         mode_specific_info = ttk.Frame(mid_frame)
         mode_specific_info.grid(column=2, row=0, sticky=(N, W), padx=16, pady=16)
-        ttk.Label(mode_specific_info, font=(BOLD_FONT, 12), text="Enemies to defeat ", foreground="#ff0808").grid(column=0, row=0, sticky=(N, W))
-        ttk.Label(mode_specific_info, font=(BOLD_FONT, 14), text=": ", foreground="#ffffff").grid(column=1, row=0, sticky=(N, W))
+        ttk.Label(mode_specific_info, font=(controller.BOLD_FONT, 12), text="Enemies to defeat ", foreground="#ff0808").grid(column=0, row=0, sticky=(N, W))
+        ttk.Label(mode_specific_info, font=(controller.BOLD_FONT, 14), text=": ", foreground="#ffffff").grid(column=1, row=0, sticky=(N, W))
 
         # cave information
-        cave_frame = ttk.Frame(self.root)
+        cave_frame = ttk.Frame(self)
         cave_frame.grid(column=0, row=2, padx=15, pady=16, sticky=(N, W))
 
-        ttk.Label(cave_frame, font=NORMAL_FONT, text="----------").grid(column=0, row=0, sticky=(N, W))
-        ttk.Label(cave_frame, font=(BOLD_FONT, 13), text="Solace", foreground="#eb3636").grid(column=0, row=1, sticky=(N, W))
-        ttk.Label(cave_frame, font=NORMAL_FONT, text="----------").grid(column=0, row=2, sticky=(N, W))
+        ttk.Label(cave_frame, font=controller.NORMAL_FONT, text="----------").grid(column=0, row=0, sticky=(N, W))
+        ttk.Label(cave_frame, font=(controller.BOLD_FONT, 13), text="Solace", foreground="#eb3636").grid(column=0, row=1, sticky=(N, W))
+        ttk.Label(cave_frame, font=controller.NORMAL_FONT, text="----------").grid(column=0, row=2, sticky=(N, W))
 
         # command line
-        command_frame = ttk.Frame(self.root)
+        command_frame = ttk.Frame(self)
         command_frame.grid(column=0, row=3, padx=10, pady=10, sticky=(S, W))
 
         self.command = StringVar()
@@ -130,14 +158,23 @@ class interface:
         self.command_line.grid(column=1, row=1, sticky=(S, W))
         
         self.command_line.focus()
-        self.root.bind("<Return>", self.send_command)
+        self.bind("<Return>", self.send_command)
 
-
-            
-    def run_mainloop(self):
-        self.root.mainloop()
-        
     def send_command(self, *args):
         self.health.set(str(self.command.get()))
         self.command_line.delete(0, tk.END)
-
+        
+class MenuFrame(tk.Frame):
+    def __init__(self, parent, controller):
+        tk.Frame.__init__(self, parent)
+        
+class LoadingFrame(tk.Frame):
+    def __init__(self, parent, controller):
+        tk.Frame.__init__(self, parent)
+        
+class EndFrame(tk.Frame):
+    def __init__(self, parent, controller):
+        tk.Frame.__init__(self, parent)
+        
+window = interface()
+window.run_mainloop()
